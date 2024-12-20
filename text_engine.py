77de3ff3418a3028d0f2b5e079db3d57118a55a7
EltonPajaziti <pajazitielton002@gmail.com>
2024-12-20 20:27:06 +0100

    preprocess_text

--- text_engine.py
+++ text_engine.py
@@ -6,3 +6,8 @@ from better_profanity import profanity
 import nltk
 
 nltk.download('brown', quiet=True)
+
+def preprocess_text():
+    # Përpunimi i korpusit duke përdorur list comprehensions për performancë më të mirë
+    return [[word.lower() for word in sentence if word.isalpha()]
+            for sentence in brown.sents() if len(sentence) > 1]
